44d416256e7edbf0b23bd9eef34a4daf555f7cc4
Joungigi04 <agency@buzzchater.com>
2025-04-28 00:20:48 +0200

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,20 +4,22 @@ from selenium.common.exceptions import NoSuchElementException
 
 def scrape_product(url: str) -> dict:
     """
-    Otwiera stronę pod podanym URL-em w headless Chrome,
-    odczytuje tytuł i cenę produktu (CSS-selectory dostosuj do swojej strony)
-    i zwraca słownik z wynikami.
+    Otwiera stronę pod podanym URL-em w headless Chromium,
+    odczytuje tytuł i cenę produktu i zwraca słownik z wynikami.
     """
-    # konfiguracja headless Chrome
+
+    # konfiguracja headless Chromium
     options = Options()
+    # tutaj wskazujemy lokalizację binarki Chromium w kontenerze
+    options.binary_location = "/usr/bin/chromium"
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+
     driver = webdriver.Chrome(options=options)
 
     try:
         driver.get(url)
-        # poczekaj chwilę, jeśli strona ładuje się dynamicznie
         driver.implicitly_wait(5)
 
         # odczyt tytułu
@@ -34,15 +36,11 @@ def scrape_product(url: str) -> dict:
         except NoSuchElementException:
             price = None
 
-        # (opcjonalnie) inne pola, np. opis, SKU, obraz:
-        # img_el = driver.find_element("css selector", "img.main-image")
-        # img_url = img_el.get_attribute("src")
-
         return {
             "url": url,
             "title": title,
             "price": price,
-            # "imageUrl": img_url,
         }
+
     finally:
         driver.quit()
